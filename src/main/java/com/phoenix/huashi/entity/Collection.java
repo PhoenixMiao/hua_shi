0674f9ab1daeb85ed2dd142756ee39ff286e334a
苏素速俗苏 <3315938098@qq.com>
2022-01-20 14:52:34 +0800

    实体修改

---
+++ src/main/java/com/phoenix/huashi/entity/Collection.java
@@ -0,0 +1,31 @@
+package com.phoenix.huashi.entity;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Id;
+import java.security.Timestamp;
+import java.time.Year;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ApiModel("Collection 收藏")
+public class Collection {
+    @Id
+    @ApiModelProperty("项目id")
+    private Long id;
+
+    @ApiModelProperty("展示项目id")
+    private Long recruitProjectId;
+
+    @ApiModelProperty("用户创赛号")
+    private String chuangNum;
+
+    @ApiModelProperty("收藏时间")
+    private String collectTime;
+}
---
+++ src/main/java/com/phoenix/huashi/entity/Member.java
@@ -0,0 +1,31 @@
+package com.phoenix.huashi.entity;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ApiModel("Member 成员")
+public class Member {
+    @Id
+    @ApiModelProperty("Id")
+    private Long id;
+
+    @ApiModelProperty("类别")
+    private String type;
+
+    @ApiModelProperty("招募项目id")
+    private Long recruitProjectId;
+
+    @ApiModelProperty("创赛号")
+    private String chuangNum;
+
+
+}
