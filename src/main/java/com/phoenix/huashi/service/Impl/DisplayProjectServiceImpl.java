641c694a6203d67f71a41a514c0b0171655d7d88
ZhuYan <2356773295@qq.com>
2022-01-28 11:12:20 +0800

    redis优化点赞

--- src/main/java/com/phoenix/huashi/service/Impl/DisplayProjectServiceImpl.java
+++ src/main/java/com/phoenix/huashi/service/Impl/DisplayProjectServiceImpl.java
@@ -14,7 +14,13 @@ import com.phoenix.huashi.entity.DisplayProject;
 import com.phoenix.huashi.mapper.DisplayProjectMapper;
 import com.phoenix.huashi.mapper.LikesMapper;
 import com.phoenix.huashi.service.DisplayProjectService;
+import com.phoenix.huashi.util.RedisUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -22,6 +28,7 @@ import javax.validation.constraints.NotNull;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DisplayProjectServiceImpl implements DisplayProjectService {
@@ -31,15 +38,57 @@ public class DisplayProjectServiceImpl implements DisplayProjectService {
     @Autowired
     private DisplayProjectMapper displayProjectMapper;
 
+    @Autowired
+    private RedisUtils redisUtil;
+
+    private static String LIKE_KEY(Long id){
+        return "redisdemo:likes:" + id;
+    }
+
+    private Long getLikesFromRedis(Long id){
+        Long likes;
+        try{
+            likes = (Long) redisUtil.get(LIKE_KEY(id));
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+        return likes;
+    }
+
+
     @Override
-    public void giveLike(Long projectId,Long userId)
+    public Long getLike(Long projectId){
+        Long likes = getLikesFromRedis(projectId);
+        if(likes != null)return likes;
+        likes = Optional.ofNullable(displayProjectMapper.getLikes(projectId)).orElse(0L);
+        redisUtil.set(LIKE_KEY(projectId),likes);
+        return likes;
+    }
+
+    @Scheduled(cron = "0 0 0 * * ?")
+    @Scheduled(cron = "0 0 0 */1 * ?")
+    public void likes2Mysql() {
+        Long likes;
+        try{
+            likes = (Long)redisUtil.get("redisdemo:likes:1");
+            if(likes != null){
+                displayProjectMapper.giveLike(likes,1L);
+            }
+        }catch (Exception e){
+        }
+    }
+
+    @Override
+    public void giveLike(Long projectId)
     {
-        Date date = new Date( );
-        SimpleDateFormat dateFormat = new SimpleDateFormat ("yyyy-MM-dd hh:mm:ss");
-        String time=dateFormat.format(date);
-        likesMapper.addToLikes(projectId,userId,time);
-        Long likesNumber = displayProjectMapper.getLikes(projectId);
-        displayProjectMapper.giveLike(likesNumber+1,projectId);
+        Long likes = getLikesFromRedis(projectId);
+        if(likes != null){
+            redisUtil.set(LIKE_KEY(projectId),likes + 1);
+        }else {
+            likes = Optional.ofNullable(displayProjectMapper.getLikes(projectId)).orElse(0L);
+            redisUtil.set(LIKE_KEY(projectId), likes + 1);
+        }
     }
     @Override
     public DisplayProject getDisplayProjectById(Long id, Long userId){
