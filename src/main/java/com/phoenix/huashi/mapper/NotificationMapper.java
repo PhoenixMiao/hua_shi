b51883c10807cc60e78b07f7e203344d9b05ff3c
苏素速俗苏 <3315938098@qq.com>
2022-02-10 18:08:28 +0800

    补充notification

--- src/main/java/com/phoenix/huashi/mapper/NotificationMapper.java
+++ src/main/java/com/phoenix/huashi/mapper/NotificationMapper.java
@@ -17,4 +17,6 @@ public interface NotificationMapper extends  MyMapper<Notification>{
      List<BriefNotification> getBriefNotificationList(@Param("type") String type);
 
 
+    @Select("SELECT id,title,source,publish_date FROM notification ")
+    List<BriefNotification> getALLBriefNotificationList();
 }
